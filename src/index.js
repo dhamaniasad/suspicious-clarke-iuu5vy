8fb72defdd5e5d46579c058cc174f8c345386939
Asad Dhamani <dhamaniasad@gmail.com>
2022-02-26 20:47:27 +0530

    WIP

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
+<h2>Hello world!</h2>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
